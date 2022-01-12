09ee5e5dfc427a05be47123ddbda43390a014e4c
vpoesmann-bts <vpoesmann-bts@gmail.com>
2022-01-12 11:55:12 +0100

    Fix card generation, add html card display

--- index.ts
+++ index.ts
@@ -15,8 +15,16 @@ enum Valeur {
 
 let jeuCartes: number[][] = []
 
+let newDiv = document.createElement("div");
+document.body.appendChild(newDiv);
+newDiv.classList.add("carte");
+newDiv.classList.add("rouge");
+
 for (let i: number = 0 ; i < 4 ; i++) {
   for (let j: number = 0 ; j < 15 ; j++) {
+    if (j > 0 && j < 13) {
+      jeuCartes.push([j, i]) 
+    }
     jeuCartes.push([j, i]) 
   }
 }
