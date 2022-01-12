5ed46a16570c5e703e53b65d99bc89637eaef101
vpoesmann-bts <vpoesmann-bts@gmail.com>
2022-01-12 11:33:26 +0100

    First card generation

--- index.ts
+++ index.ts
@@ -1,1 +1,24 @@
-console.log("hello world")
+enum Couleur {
+  Rouge,
+  Vert,
+  Jaune,
+  Bleu,
+}
+
+enum Valeur {
+  Plus2 = 10,
+  Inversion,
+  Passe,
+  Joker,
+  Plus4,
+}
+
+let jeuCartes: number[][] = []
+
+for (let i: number = 0 ; i < 4 ; i++) {
+  for (let j: number = 0 ; j < 15 ; j++) {
+    jeuCartes.push([j, i]) 
+  }
+}
+
+console.log(jeuCartes)
